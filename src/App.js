768bc32cacdc247dccd303e7463a59df629bde80
Maxwell Shick <maxwellshick@gmail.com>
2022-04-01 17:36:01 -0400

    Reworked app file, added the .env file with API credentials

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
 import './App.css';
+import React, { useEffect, useState } from "react";
+
+const [lat, setLat] = useState([]);
+const [long, setLong] = useState([]);
+
+useEffect(() => {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    setLat(position.coords.latitude);
+    setLong(position.coords.longitude);
+  });
+
+  console.log("Latitude is:", lat);
+  console.log("Longitude is:", long);
+}, [lat, long]);
 
 function App() {
   return (
