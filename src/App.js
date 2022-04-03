e556c7147d8b39b954d8b922acde3b6194d52309
Maxwell Shick <maxwellshick@gmail.com>
2022-04-03 18:55:44 -0400

    Changing to Axios for the API

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
 import Weather from './Components/weather.js';
+import { Dimmer, Loader } from 'semantic-ui-react';
+import axios from 'axios';
 
 function App() {
 
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
+  const [lat, setLat] = useState(null);
+  const [long, setLong] = useState(null);
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        setLat(position.coords.latitude);
-        setLong(position.coords.longitude);
-      });
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
 
-      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-        .then(res => JSON.stringify(res))
-        .then(result => {
-          setData(result)
-          console.log(result);
-        });
-    }
-    fetchData();
+    axios.get(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then((weatherData) => {
+        setData(weatherData)
+        if (lat > 0 && long > 0) setLoading(false);
+        console.log(weatherData);
+      });
   }, [lat, long]);
 
   return (
     <div className="App">
-      {(typeof data.main != 'undefined') ? (
+      {(!loading) ? (
         <Weather weatherData={data} />
       ) : (
-        <div></div>
+        <div>
+          <Dimmer active>
+            <Loader>Loading...</Loader>
+          </Dimmer>
+        </div>
       )}
     </div>
   );
